8d698968b4aac0188ade939af6e701e810b80a67
adrian <adrian@prohm.de>
2024-04-25 23:33:41 +0200

    /lay vorbereitet

---
+++ src/de/newrp/Player/LayCommand.java
@@ -0,0 +1,16 @@
+package de.newrp.Player;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class LayCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender cs, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
+        Player p = (Player) cs;
+        
+        return false;
+    }
+}
