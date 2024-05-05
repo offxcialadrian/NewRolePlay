e1fcd875a3faaa5d664bcd27d6882c788d0515bc
Smolder <kygott@gmx.de>
2024-05-05 02:36:47 +0200

    xd

--- src/main/java/de/newrp/discord/Discord.java
+++ src/main/java/de/newrp/discord/Discord.java
@@ -84,7 +84,7 @@ public class Discord {
         Guild guild = jda.getGuildById(guildId);
         final Optional<Member> optionalMember = guild.findMembers(a -> {
             if(a.hasPermission(Permission.ADMINISTRATOR)) {
-                Bukkit.getLogger().info("Effective: " + a.getEffectiveName() + ", Nickname: " + a.getNickname());
+                Bukkit.getLogger().info("Effective: " + a.getEffectiveName() + ", Nickname: " + a.getNickname() + ", Global Name: " + a.getUser().getGlobalName());
             }
             return a.getEffectiveName().equalsIgnoreCase(username);
         }).get().stream().findFirst();
