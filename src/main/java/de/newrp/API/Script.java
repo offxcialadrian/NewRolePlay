27fecb4ead70b6efb518d9161fcb8ca92cd5569e
Smolder <kygott@gmx.de>
2024-05-23 22:29:26 +0200

    things and stuff yk?

--- src/main/java/de/newrp/API/Script.java
+++ src/main/java/de/newrp/API/Script.java
@@ -83,7 +83,13 @@ public class Script {
             players.add(p);
         }
         //sort list by rank
-        players.sort((o1, o2) -> {
+
+        return players;
+    }
+
+    public static List<Player> getSortedNRPTeam() {
+        final List<Player> nrpTeam = getNRPTeam();
+        nrpTeam.sort((o1, o2) -> {
             if (getRank(o1).getWeight() > getRank(o2).getWeight()) {
                 return -1;
             } else if (getRank(o1).getWeight() < getRank(o2).getWeight()) {
@@ -91,7 +97,7 @@ public class Script {
             }
             return 0;
         });
-        return players;
+        return nrpTeam;
     }
 
     public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm, boolean highestfirst) {
