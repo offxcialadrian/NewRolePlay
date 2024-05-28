6d988a104d6456e77c52cd4403c72f0f906241a0
Ramses <isuramses@gmail.com>
2024-05-28 21:00:10 +0200

    BreakIn Fix

--- src/main/java/de/newrp/Organisationen/BreakIn.java
+++ src/main/java/de/newrp/Organisationen/BreakIn.java
@@ -89,7 +89,7 @@ public class BreakIn implements Listener {
             public void run() {
                 if(p.getLocation().distance(house.getSignLocation()) > 3) {
                     p.sendMessage(PREFIX + "Du hast den Einbruch abgebrochen.");
-                    p.removePotionEffect(PotionEffectType.SLOW);
+                    if (p.hasPotionEffect(PotionEffectType.SLOW)) p.removePotionEffect(PotionEffectType.SLOW);
                     progress.remove(p.getName());
                     this.cancel();
                     return;
@@ -97,7 +97,7 @@ public class BreakIn implements Listener {
 
                 if(!p.getInventory().getItemInMainHand().equals(brechstange)) {
                     p.sendMessage(PREFIX + "Du hast den Einbruch abgebrochen.");
-                    p.removePotionEffect(PotionEffectType.SLOW);
+                    if (p.hasPotionEffect(PotionEffectType.SLOW)) p.removePotionEffect(PotionEffectType.SLOW);
                     progress.remove(p.getName());
                     this.cancel();
                     return;
@@ -105,7 +105,7 @@ public class BreakIn implements Listener {
 
                 if(Handschellen.isCuffed(p)) {
                     p.sendMessage(PREFIX + "Du hast den Einbruch abgebrochen.");
-                    p.removePotionEffect(PotionEffectType.SLOW);
+                    if (p.hasPotionEffect(PotionEffectType.SLOW)) p.removePotionEffect(PotionEffectType.SLOW);
                     progress.remove(p.getName());
                     this.cancel();
                     return;
@@ -113,7 +113,7 @@ public class BreakIn implements Listener {
 
                 if(!COOLDOWNS.containsKey(p.getName())) {
                     p.sendMessage(PREFIX + "Du hast den Einbruch abgebrochen.");
-                    p.removePotionEffect(PotionEffectType.SLOW);
+                    if (p.hasPotionEffect(PotionEffectType.SLOW)) p.removePotionEffect(PotionEffectType.SLOW);
                     progress.remove(p.getName());
                     this.cancel();
                     return;
