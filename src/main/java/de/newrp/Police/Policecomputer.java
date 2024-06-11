b3f795990925f96d96b4d430fc2c018a1a14dfba
Ramses <isuramses@gmail.com>
2024-06-11 19:18:17 +0200

    jail fix

--- src/main/java/de/newrp/Police/Policecomputer.java
+++ src/main/java/de/newrp/Police/Policecomputer.java
@@ -57,11 +57,11 @@ public static HashMap<String, Long> cooldown = new HashMap<>();
         }
 
         Inventory inv = Bukkit.createInventory(null, 9, "§8[§9Polizeicomputer§8] §9" + tg.getName());
-        inv.setItem(0, new ItemBuilder(Material.PLAYER_HEAD).setName("Personendaten").setLore("§8» §7Klicke um die Personendaten von " + Script.getName(tg) + " zu sehen.").build());
-        inv.setItem(1, new ItemBuilder(Material.PAPER).setName("Lizenzen").setLore("§8» §7Klicke um die Lizenzen von " + Script.getName(tg) + " zu sehen.").build());
-        inv.setItem(2, new ItemBuilder(Material.NETHER_STAR).setName("Gefährlichkeitsstufe").setLore("§8» §7Gefährlichkeitsstufe von " + Script.getName(tg) + "§8: §7" + getDangerLevel(tg)).build());
-        if (Beruf.getBeruf(p) == Beruf.Berufe.BUNDESNACHRICHTENDIENST) inv.setItem(3, new ItemBuilder(Material.COMPASS).setName("Orten").setLore("§8» §7Klicke um " + Script.getName(tg) + " zu orten.").build());
-        inv.setItem(Beruf.getBeruf(p) == Beruf.Berufe.BUNDESNACHRICHTENDIENST ? 4 : 3, new ItemBuilder(Material.OAK_SIGN).setName("Gesucht?").setLore("§8» " + (Fahndung.isFahnded(tg) ? "§cGesucht" : "§aNicht gesucht")).build());
+        inv.setItem(0, new ItemBuilder(Material.PLAYER_HEAD).setName("§ePersonendaten").setLore("§8» §7Klicke um die Personendaten von " + Script.getName(tg) + " zu sehen.").build());
+        inv.setItem(1, new ItemBuilder(Material.PAPER).setName("§eLizenzen").setLore("§8» §7Klicke um die Lizenzen von " + Script.getName(tg) + " zu sehen.").build());
+        inv.setItem(2, new ItemBuilder(Material.NETHER_STAR).setName("§eGefährlichkeitsstufe").setLore("§8» §7Gefährlichkeitsstufe von " + Script.getName(tg) + "§8: §7" + getDangerLevel(tg)).build());
+        if (Beruf.getBeruf(p) == Beruf.Berufe.BUNDESNACHRICHTENDIENST) inv.setItem(3, new ItemBuilder(Material.COMPASS).setName("§eOrten").setLore("§8» §7Klicke um " + Script.getName(tg) + " zu orten.").build());
+        inv.setItem(Beruf.getBeruf(p) == Beruf.Berufe.BUNDESNACHRICHTENDIENST ? 4 : 3, new ItemBuilder(Material.OAK_SIGN).setName("§eGesucht?").setLore("§8» " + (Fahndung.isFahnded(tg) ? "§cGesucht" : "§aNicht gesucht")).build());
         p.openInventory(inv);
 
         Beruf.Berufe.POLICE.sendMessage(PREFIX + Script.getName(p) + " fragt die Daten von " + Script.getName(tg) + " ab.");
