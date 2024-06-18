448bc9c620d7ed0faece6df4e5ca288ae5dbbd70
Ramses <isuramses@gmail.com>
2024-06-18 23:18:58 +0200

    anti -gfb

--- src/main/java/de/newrp/API/PayDay.java
+++ src/main/java/de/newrp/API/PayDay.java
@@ -59,8 +59,8 @@ public class PayDay extends BukkitRunnable {
             double lohnsteuer = Steuern.Steuer.LOHNSTEUER.getPercentage();
             double gfb_lohnsteuer = Steuern.Steuer.GFB_LOHNSTEUER.getPercentage();
             double krankenversicherung = Steuern.Steuer.KRANKENVERSICHERUNG.getPercentage();
-            if (BeziehungCommand.isMarried(p)) lohnsteuer = lohnsteuer - 5.0;
-            if (BeziehungCommand.isMarried(p)) gfb_lohnsteuer = gfb_lohnsteuer - 5.0;
+            if (BeziehungCommand.isMarried(p)) lohnsteuer = Math.max(0.0, lohnsteuer - 5.0);
+            if (BeziehungCommand.isMarried(p)) gfb_lohnsteuer = Math.max(0.0, gfb_lohnsteuer - 5.0);
             p.sendMessage("§9=== §l§ePayDay §9===");
             p.sendMessage("§8" + Messages.ARROW + " §7Kontostand: " + (Script.getMoney(p, PaymentType.BANK) >= 0 ? "§a" : "§c") + Script.getMoney(p, PaymentType.BANK) + "€");
             p.sendMessage("§8" + Messages.ARROW + " §7Kontoführungsgebühr: §c-" + Banken.getBankByPlayer(p).getKontoKosten() + "€");
