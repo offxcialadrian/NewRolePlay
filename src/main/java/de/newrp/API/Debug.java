e8eaefb1b627165b02d7c6b8870d3d6a939e01f6
kybu <kygott@gmx.de>
2024-06-22 19:53:37 +0200

    removed debug

--- src/main/java/de/newrp/API/Debug.java
+++ src/main/java/de/newrp/API/Debug.java
@@ -1,12 +1,10 @@
 package de.newrp.API;
 
 import de.newrp.Administrator.Notifications;
-import org.bukkit.Bukkit;
 
 public class Debug {
 
     public static void debug(Object msg) {
-        Bukkit.getLogger().info("DEBUG: " + msg);
        Notifications.sendMessage(Notifications.NotificationType.DEBUG, "§e" + msg);
     }
 
